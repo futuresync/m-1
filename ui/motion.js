f69ad1bee9c2258f60656aee37277b59b5f90c4f
YI-MOTION <103293810+yi-motion@users.noreply.github.com>
2023-04-02 09:08:14 +0000

    v0.4

--- ui/motion.js
+++ ui/motion.js
@@ -1,8 +1,10 @@
 // fake loader
 $(".load").delay(2000).fadeOut();
+$(".userUI").hide();
 
 // Animation message
 function MotionMessage() {
+    $(".userUI").fadeIn();
     $(".aiUI").hide(1, function () {
         $(".aiUI").slideDown(600);
     });
